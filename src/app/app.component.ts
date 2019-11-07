cf368358fb65c7cabd83a0fe8a72d177215c89ef
sowmya398 <sowmya.m90@wipro.com>
2019-11-07 14:33:01 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import{Title}from '@angular/platform-browser';
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'Angular';
-}
+ 
+
+
+  }
+
